440836e0210f5eb716cf3c3c01186e5993533f70
Raif Shareef <53454000+raif-sh@users.noreply.github.com>
2024-06-03 14:35:06 +0000

    adding meals data

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+MEALS = [
+  {
+    'id': 1,
+    'name': 'Pizza',
+    'ingredients':
+    {
+      'dough': '1 cup',
+      'sauce': '100 ml',
+      'cheese': '50 gram',
+      'toppings': '300 gram'
+    },
+    'link': 'https://www.pizzahut.co.uk/pizza/pizza-hut-pizza'
+  },
+  {
+    'id': 2,
+    'name': 'Burger',
+    'ingredients':
+    {
+      'meat': '1 kg',
+      'vegetables': '1 kg',
+      'sauce': '100 ml',
+      'buns': '2 pieces'
+    },
+    'link': 'https://www.burgerking.co.uk/menu/burger-king-menu'
+  }
+]
+
 
 @app.route("/")
-def hello_world():
-  return "Hello jo!"
+def index():
+  return render_template("home.html", meals=MEALS)
 
+@app.route("/api/meals")
+def list_meals():
+  return jsonify(MEALS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
